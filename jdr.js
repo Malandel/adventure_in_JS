1d2f8f2c028c3a1082f9684be2799db4d1fb52da
Malandel <marin.guerie@gmail.com>
2020-10-07 12:45:59 +0200

    deuxieme ajout

--- jdr.js
+++ jdr.js
@@ -1,67 +1,153 @@
 //Alert for the Character's name
-let nameAlert = document.querySelector("#okName")
-
-nameAlert.addEventListener('click', () =>{
-
-	let messageName = document.querySelector("#chName").value;
-	alert("Greetings " + messageName + "! \n \n Choose wisely.");
+function validateName(){
+	let messageName = document.getElementById("chName").value;
+  if (messageName==""){
+    alert("Greetings Random Stranger ! \n \n You forgot to chose a name.");
+  }
+  else {alert("Greetings " + messageName + " ! \n \n Choose wisely.");
+        afficherName;}
 }
-)
+
 
 //Alert for the City's name
-let cityAlert = document.querySelector("#okCityName")
-
-cityAlert.addEventListener('click', () =>{
-
-	let messageCity = document.querySelector("#cityName").value;
-	let messageName = document.querySelector("#chName").value;
-	alert(messageName + ", you're from " + messageCity + " ! \n \n Please continue.");
+function validateCityName(){
+	let messageCity = document.getElementById("cityName").value;
+	let messageName = document.getElementById("chName").value;
+  if (messageName=="", messageCity==""){
+    alert("Greetings Random Stranger, you're from the Invisible City ! \n \n You forgot to name your city.");
+  }
+	else alert(messageName + ", you're from " + messageCity + " ! \n \n Please continue.");
 }
-)
+
 
 //Alert for the Pet's name
-let petAlert = document.querySelector("#okPetName")
-
-petAlert.addEventListener('click', () =>{
-
-	let messagePet = document.querySelector("#petName").value;
-	let messageName = document.querySelector("#chName").value;
-	let messageCity = document.querySelector("#cityName").value;
-	alert(messageName + ", from " + messageCity + ", you named your pet " + messagePet + " ! \n \n Please continue.");
-}
-)
-
-//SUM STATS
-
-let 
-
-
-//Show select weapons
-var FormStuff = {
-  
-  init: function() {
-    this.applyConditionalRequired();
-    this.bindUIActions();
-  },
-  
-  bindUIActions: function() {
-    ("input[type='radio']").on("change", this.applyConditionalRequired);
-  },
-  
-  applyConditionalRequired: function() {
-    
-    (".require-if-active").each(function() {
-      var el = (this);
-      if ((el.data("require-pair")).is(":checked")) {
-        el.prop("required", true);
-      } else {
-        el.prop("required", false);
-      }
-    });
-    
+function validatePetName(){
+	let messagePet = document.getElementById("petName").value;
+	let messageName = document.getElementById("chName").value;
+	let messageCity = document.getElementById("cityName").value;
+  if (messageName=="", messageCity=="", messagePet==""){
+    alert("Greetings Random Stranger, you're from the Invisible City, you named your pet NotThere ! \n \n You forgot to name your pet.");
   }
-  
-};
+  else {alert(messageName + ", from " + messageCity + ", you named your pet " + messagePet + " ! \n \n Please continue.");}
+}
 
-FormStuff.init();
+
+
+//STATS
+let techPoints = 0
+let magicPoints = 0
+let stealthPoints = 0
+let strenghtPoints = 0 
+
+function validateCity() {
+  if (document.getElementById("techCity").checked == true){
+        techPoints += 5;
+        console.log("techPoints "+techPoints);
+  }
+  else if (document.getElementById("castleCity").checked == true){
+        strenghtPoints += 5;
+        console.log("strenghtPoints "+strenghtPoints);
+
+  }
+  else if (document.getElementById("forestCity").checked == true){
+        stealthPoints += 5;
+        console.log("stealthPoints "+stealthPoints);
+  }
+  else if (document.getElementById("magicCity").checked == true){
+        magicPoints += 5;
+        console.log("magicPoints "+magicPoints);
+  }
+}
+
+function validateRace() {
+  if (document.getElementById("dwarf").checked == true){
+        techPoints += 3;
+        console.log("techPoints "+techPoints);
+  }
+  else if (document.getElementById("orc").checked == true){
+        strenghtPoints += 3;
+        console.log("strenghtPoints "+strenghtPoints);
+
+  }
+  else if (document.getElementById("human").checked == true){
+        stealthPoints += 3;
+        console.log("stealthPoints "+stealthPoints);
+  }
+  else if (document.getElementById("elf").checked == true){
+        magicPoints += 3;
+        console.log("magicPoints "+magicPoints);
+  }
+}
+
+function validatePet() {
+  if (document.getElementById("crow").checked == true){
+        techPoints += 1;
+        console.log("techPoints "+techPoints);
+  }
+  else if (document.getElementById("bear").checked == true){
+        strenghtPoints += 1;
+        console.log("strenghtPoints "+strenghtPoints);
+
+  }
+  else if (document.getElementById("rat").checked == true){
+        stealthPoints += 1;
+        console.log("stealthPoints "+stealthPoints);
+  }
+  else if (document.getElementById("phoenix").checked == true){
+        magicPoints += 1;
+        console.log("magicPoints "+magicPoints);
+  }
+}
+
+function validateWeapon() {
+  if (document.getElementById("gauntlet-basic").checked == true){
+        techPoints += 4;
+        console.log("techPoints "+techPoints);
+        afficherTech(techPoints);
+  }
+  else if (document.getElementById("sword-basic").checked == true){
+        strenghtPoints += 4;
+        console.log("strenghtPoints "+strenghtPoints);
+        afficherStr(strenghtPoints);
+
+  }
+  else if (document.getElementById("bow-basic").checked == true){
+        stealthPoints += 4;
+        console.log("stealthPoints "+stealthPoints);
+        afficherSth(stealthPoints);
+  }
+  else if (document.getElementById("spellbook-basic").checked == true){
+        magicPoints += 4;
+        console.log("magicPoints "+magicPoints);
+        afficherMag(magicPoints);
+  }
+}
+
+//SHOW STATS - I have to create div to show results
+function afficherTech(sumTech){
+  console.log(techPoints);
+  document.getElementById("techResult").innerHTML=sumTech;
+}
+function afficherStr(sumStr){
+  console.log(strenghtPoints);
+  document.getElementById("strResult").innerHTML=sumStr;
+}
+function afficherTech(sumSth){
+  console.log(stealthPoints);
+  document.getElementById("sthResult").innerHTML=sumSth;
+}
+function afficherTech(sumMag){
+  console.log(magicPoints);
+  document.getElementById("magResult").innerHTML=sumMag;
+}
+
+
+//SHOW NAME - EVENT "Continue" Button - need to link the other page
+let sumName = document.getElementById(chName).value
+function afficherName(sumName){
+  console.log(sumName);
+  document.getElementById("name-character").innerHTML=sumName;
+}
+
+
 
